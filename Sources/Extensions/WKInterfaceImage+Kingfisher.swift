ab30d912301c532b16eaa80ca4f162eed3398d7b
Leonardo de Matos <leonardo.dematossouza@gmail.com>
2019-07-08 11:50:55 -0300

    adding canImport for WatchKit xcode 11

--- Sources/Extensions/WKInterfaceImage+Kingfisher.swift
+++ Sources/Extensions/WKInterfaceImage+Kingfisher.swift
@@ -24,7 +24,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-import WatchKit
+#if canImport(WatchKit)
 
 extension KingfisherWrapper where Base: WKInterfaceImage {
 
